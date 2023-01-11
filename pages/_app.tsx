2ee91cab397239e8f3dacc26f65843b4b117d70f
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2023-01-11 21:10:33 +0330

    tabler and bootstrap styles added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/tabler.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
