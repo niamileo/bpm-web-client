407ad85dd44b28dd71645f654501d1a6df5d46db
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2023-01-17 19:51:30 +0330

    _app optimized

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,21 @@
+import type { AppProps } from "next/app";
+// ** Partials
+import Footer from "../components/partials/Footer";
+import Header from "components/partials/Header";
+import TopNav from "components/partials/TopNav";
+// ** Styles
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/tabler.css";
-import type { AppProps } from "next/app";
-
-import Aside from "../components/partials/aside";
-import Kapak from "../components/partials/aside/kapak";
-import Footer from "../components/partials/footer";
-import IndexRest from "../components/partials/indexpage/rest";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="page">
-      <Aside />
+      <Header />
+      <TopNav />
       <div className="content">
         <div className="container-fluid">
-          {/* <!-- Page title --> */}
-          {/* <Kapak /> */}
           <Component {...pageProps} />
-          {/* <IndexRest /> */}
         </div>
         <Footer />
       </div>
