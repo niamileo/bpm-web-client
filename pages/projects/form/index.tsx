f0e8716fb912d3f71265626f859680e109f00547
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2023-01-18 17:05:45 +0330

    project form added

---
+++ pages/projects/form/index.tsx
@@ -0,0 +1,34 @@
+import FormikTextField from "components/form/TextField/formik";
+import PageHeader from "components/partials/PageHeader";
+import { Form, Formik } from "formik";
+import { Button, Card, CardBody, Row } from "reactstrap";
+import { createProject } from "services/ProjectService";
+
+export default function ProjectForm() {
+  const handleSubmit = (data: object) => {
+    createProject(data);
+  };
+
+  return (
+    <Row>
+      <PageHeader title="Project Form" />
+      <Card>
+        <CardBody>
+          <Formik initialValues={{}} onSubmit={handleSubmit}>
+            {({ values }) => (
+              <Form>
+                <FormikTextField name="title" label="Title" />
+                <FormikTextField
+                  name="desc"
+                  label="Description"
+                  type="textarea"
+                />
+                <Button type="submit">Create</Button>
+              </Form>
+            )}
+          </Formik>
+        </CardBody>
+      </Card>
+    </Row>
+  );
+}
