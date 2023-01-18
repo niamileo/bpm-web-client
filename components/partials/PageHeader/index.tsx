71d40289c5e80c3715f410af1416dd4e07a9d4a7
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2023-01-18 17:04:34 +0330

    PageHeader props added

--- components/partials/PageHeader/index.tsx
+++ components/partials/PageHeader/index.tsx
@@ -1,24 +1,26 @@
 import Plus from "components/SVG/Plus.svg";
 import { Button, Col } from "reactstrap";
 
-export default function Kapak() {
+type PageHeaderProps = {
+  title?: string;
+  pretitle?: string;
+  children?: any;
+};
+
+export default function PageHeader({
+  children,
+  title,
+  pretitle,
+}: PageHeaderProps) {
   return (
     <div className="page-header d-print-none">
       <div className="row align-items-center">
         <Col>
-          <div className="page-pretitle">Overview</div>
-          <h2 className="page-title">Fluid vertical layout</h2>
+          <div className="page-pretitle">{pretitle}</div>
+          <h2 className="page-title">{title}</h2>
         </Col>
         <div className="col-auto ms-auto d-print-none">
-          <div className="btn-list">
-            <span className="d-none d-sm-inline">
-              <Button color="white">New View</Button>
-            </span>
-            <Button color="primary">
-              <Plus />
-              Create new report
-            </Button>
-          </div>
+          <div className="btn-list">{children}</div>
         </div>
       </div>
     </div>
