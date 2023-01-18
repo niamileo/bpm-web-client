f25f6b9958bba0d2b3b53b20a9dab9a12357fdd8
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2023-01-18 17:05:19 +0330

    Project Services added

---
+++ services/ProjectService.ts
@@ -0,0 +1,14 @@
+import http from "services/httpService";
+
+type FetchProjectsProps = {};
+
+type CreateProjectsProps = {
+  title?: string;
+  desc?: string;
+};
+
+export const fetchProjects = (params: FetchProjectsProps) =>
+  http.get("projects", { params });
+
+export const createProject = (data: CreateProjectsProps) =>
+  http.post("projects", data);
