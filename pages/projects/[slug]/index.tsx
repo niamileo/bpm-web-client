b8795f753d4bcce92b2ee6182156b06e1df3a926
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2023-01-19 17:57:07 +0330

    project dashboard added

--- pages/projects/[slug]/index.tsx
+++ pages/projects/[slug]/index.tsx
@@ -1,13 +1,33 @@
-import { Row } from "reactstrap";
-import Chart from "../../../components/widgets/Chart";
-import Tasks from "../../tasks";
+// ** Hooks
+import useHttp from "hooks/use-http";
+import { useRouter } from "next/router";
+// ** Components
+import PageHeader from "components/partials/PageHeader";
+import { Button, Nav, Row } from "reactstrap";
+// ** Services
+import { fetchProjectById } from "services/ProjectService";
 
 export default function ProjectDashboard() {
+  const router = useRouter();
+  const { data } = useHttp(fetchProjectById, { id: router.query.id }, {});
+
   return (
     <Row>
-      <h1>project dashboard</h1>
-      <Chart />
-      <Tasks />
+      <PageHeader
+        title={data.length && data[0].title}
+        pretitle="Project Dashboard"
+      />
+      <Nav className="mb-5">
+        <Button color="info" className="btn-pill ms-2" outline>
+          General
+        </Button>
+        <Button color="info" className="btn-pill ms-2" outline>
+          Users
+        </Button>
+        <Button color="info" className="btn-pill ms-2" outline>
+          Tasks
+        </Button>
+      </Nav>
     </Row>
   );
 }
