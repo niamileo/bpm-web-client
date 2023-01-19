a3d3c99b2fc51bb1e9953495910d377073e1e029
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2023-01-19 11:58:53 +0330

    fix -> httpService configs that caused CORS error removed

--- services/httpService/index.ts
+++ services/httpService/index.ts
@@ -4,9 +4,7 @@ export const BASE_URL = "http://localhost:8000";
 
 export const apiUrlV1 = `${BASE_URL}`;
 
-axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.baseURL = apiUrlV1;
-axios.defaults.withCredentials = true;
 axios.interceptors.response.use(null, (error) => {
   // prettier-ignore
   const expectedError =
@@ -21,17 +19,17 @@ axios.interceptors.response.use(null, (error) => {
   return Promise.reject(error);
 });
 
-axios.interceptors.request.use(function (config) {
-  const url = config.url;
+// axios.interceptors.request.use(function (config) {
+//   const url = config.url;
 
-  // ** removing trailing slash from end of url
-  // ** this little slash (which seems its called trailing), causes CORS error
-  const lastChar = url[url.length - 1];
-  if (lastChar === "/") {
-    config.url = config.url.replace(/\/$/, "");
-  }
-  return config;
-});
+//   // ** removing trailing slash from end of url
+//   // ** this little slash (which seems its called trailing), causes CORS error
+//   const lastChar = url[url.length - 1];
+//   if (lastChar === "/") {
+//     config.url = config.url.replace(/\/$/, "");
+//   }
+//   return config;
+// });
 
 export default {
   get: axios.get,
