2a372e799df2fb287398913c6f553f015748b81d
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2023-01-19 17:56:48 +0330

    getProjectById service added

--- services/ProjectService.ts
+++ services/ProjectService.ts
@@ -1,6 +1,9 @@
 import http from "services/httpService";
 
 type FetchProjectsProps = {};
+type FetchProjectByIdProps = {
+  id?: string | number;
+};
 
 type CreateProjectsProps = {
   title?: string;
@@ -9,6 +12,9 @@ type CreateProjectsProps = {
 
 export const fetchProjects = (params?: FetchProjectsProps) =>
   http.get("projects", { params });
+
+export const fetchProjectById = ({ id }: FetchProjectByIdProps) =>
+  http.get(`project/${id}`);
 
 export const createProject = (data: CreateProjectsProps) =>
   http.post("project/create", data);
