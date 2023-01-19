ba21a00f50bc7af700d4b02dbbf0d48eb4460633
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2023-01-19 19:17:00 +0330

    project dashboard components added

--- pages/projects/[slug]/index.page.tsx
+++ pages/projects/[slug]/index.page.tsx
@@ -4,13 +4,18 @@ import { useRouter } from "next/router";
 // ** Components
 import PageHeader from "components/partials/PageHeader";
 import { Button, Nav, Row } from "reactstrap";
+import Link from "next/link";
+// ** Partials
+import General from "./general";
+import Tasks from "./tasks";
+import Users from "./users";
 // ** Services
 import { fetchProjectById } from "services/ProjectService";
 
 export default function ProjectDashboard() {
   const router = useRouter();
   const { data } = useHttp(fetchProjectById, { id: router.query.id }, {});
-
+  console.log(router);
   return (
     <Row>
       <PageHeader
@@ -18,16 +23,68 @@ export default function ProjectDashboard() {
         pretitle="Project Dashboard"
       />
       <Nav className="mb-5">
-        <Button color="info" className="btn-pill ms-2" outline>
-          General
-        </Button>
-        <Button color="info" className="btn-pill ms-2" outline>
-          Users
-        </Button>
-        <Button color="info" className="btn-pill ms-2" outline>
-          Tasks
-        </Button>
+        <Link
+          style={{ textDecoration: "none" }}
+          href={{
+            href: `/projects`,
+            query: {
+              id: router.query.id,
+              slug: router.query.slug,
+              mode: "general",
+            },
+          }}
+        >
+          <Button
+            color="info"
+            className="btn-pill ms-2"
+            outline={router.query.mode !== "general"}
+          >
+            General
+          </Button>
+        </Link>
+        <Link
+          style={{ textDecoration: "none" }}
+          href={{
+            href: `/projects`,
+            query: {
+              id: router.query.id,
+              slug: router.query.slug,
+              mode: "tasks",
+            },
+          }}
+        >
+          <Button
+            color="info"
+            className="btn-pill ms-2"
+            outline={router.query.mode !== "tasks"}
+          >
+            Tasks
+          </Button>
+        </Link>
+        <Link
+          style={{ textDecoration: "none" }}
+          href={{
+            href: `/projects`,
+            query: {
+              id: router.query.id,
+              slug: router.query.slug,
+              mode: "users",
+            },
+          }}
+        >
+          <Button
+            color="info"
+            className="btn-pill ms-2"
+            outline={router.query.mode !== "users"}
+          >
+            Users
+          </Button>
+        </Link>
       </Nav>
+
+      {router.query.mode === "general" && <General />}
+      {router.query.mode === "tasks" && <Tasks />}
+      {router.query.mode === "users" && <Users />}
     </Row>
   );
 }
