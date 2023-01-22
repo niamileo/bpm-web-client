5648ead3cd91d20d8f66ff30756c864d8aef9a62
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2023-01-22 22:09:01 +0330

    Project general component added

--- pages/projects/[slug]/general.tsx
+++ pages/projects/[slug]/general.tsx
@@ -1,3 +1,47 @@
+import FormikTextField from "components/form/TextField/formik";
+import { Form, Formik } from "formik";
+import useHttp from "hooks/use-http";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
+import { Button, Row } from "reactstrap";
+import { updateProject, fetchProjectById } from "services/ProjectService";
+
 export default function General() {
-  return <h1>General</h1>;
+  const router = useRouter();
+
+  const { data = [] } = useHttp<Project[]>(
+    fetchProjectById,
+    { id: router.query.id },
+    true
+  );
+
+  const handleSubmit = (data: object) => {
+    // @ts-ignore
+    updateProject({ id: router.query.id, ...data }).then(() => {
+      toast.success("Project Updated Successfully");
+    });
+  };
+
+  return (
+    <Row>
+      <Formik
+        initialValues={{
+          title: data.length && data[0].title,
+          desc: data.length && data[0].desc,
+          slug: data.length && data[0].slug,
+        }}
+        enableReinitialize
+        onSubmit={handleSubmit}
+      >
+        {() => (
+          <Form>
+            <FormikTextField name="title" />
+            <FormikTextField name="slug" />
+            <FormikTextField name="desc" type="textarea" />
+            <Button type="submit">Save</Button>
+          </Form>
+        )}
+      </Formik>
+    </Row>
+  );
 }
