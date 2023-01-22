b906a41724d42dc01f9cf248f5e766979770c4b4
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2023-01-22 21:53:32 +0330

    project users components added

--- pages/projects/[slug]/users.tsx
+++ pages/projects/[slug]/users.tsx
@@ -1,3 +1,26 @@
+import Datatable from "components/widgets/Datatable";
+import useHttp from "hooks/use-http";
+import { Col, Row } from "reactstrap";
+import { fetchProjectMembers } from "services/ProjectService";
+
 export default function Users() {
-  return <h1>Users</h1>;
+  // const {
+  //   data: [],
+  // } = useHttp<ProjectMembers[]>(fetchProjectMembers, {}, true);
+
+  return (
+    <Row>
+      <Col sm={12}>
+        <Datatable
+          rows={[]}
+          columns={[
+            { name: "id" },
+            { name: "title" },
+            { name: "desc" },
+            { name: "type" },
+          ]}
+        />
+      </Col>
+    </Row>
+  );
 }
