014985732c9a4761b4265f46530e3104f98a6877
bahare shirafkan <bahare.shirafkan13@gmail.com>
2023-02-13 03:28:28 +0330

    add task form to slug project

--- pages/projects/[slug]/index.page.tsx
+++ pages/projects/[slug]/index.page.tsx
@@ -7,10 +7,11 @@ import { Button, Nav, Row } from "reactstrap";
 import Link from "next/link";
 // ** Partials
 import General from "./general";
-import Tasks from "./tasks";
+import Tasks from "./task/list";
 import Users from "./users";
 // ** Services
 import { fetchProjectById } from "services/ProjectService";
+import TaskForm from "./task/form";
 
 export default function ProjectDashboard() {
   const router = useRouter();
@@ -49,7 +50,7 @@ export default function ProjectDashboard() {
         <Link
           style={{ textDecoration: "none" }}
           href={{
-            href: `/projects`,
+            href: `/projects/task`,
             query: {
               id: router.query.id,
               slug: router.query.slug,
@@ -89,6 +90,7 @@ export default function ProjectDashboard() {
       {router.query.mode === "general" && <General />}
       {router.query.mode === "tasks" && <Tasks />}
       {router.query.mode === "users" && <Users />}
+      {router.query.mode === "task-form" && <TaskForm />}
     </Row>
   );
 }
