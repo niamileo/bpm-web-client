2668de96ea8188bd44a7088b212dfb9a06ef68f7
bahare shirafkan <bahare.shirafkan13@gmail.com>
2023-02-13 03:29:16 +0330

    add create task to service

--- services/TaskService.ts
+++ services/TaskService.ts
@@ -2,3 +2,6 @@ import http from "services/httpService";
 
 export const fetchTasks = (params?: FetchTasksProps) =>
   http.get("tasks", { params });
+
+
+  export const createTask=(data?:CreateTaskProps)=>http.post('task/create',data)
